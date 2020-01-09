3b3041858b76f4d7f37f459d9dc6268db23b2c68
JSDV <841207@gmail.com>
2020-01-09 16:27:13 +0800

    types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { compose, last, upperFirst, toLower } from 'lodash/fp'
-import j2i from 'json-to-ts'
+import {upperFirst} from 'lodash/fp'
 import _coder from './coder';
 export const coder = _coder;
 interface RegexDict {
@@ -9,11 +8,39 @@ interface StrParameter {
     [k: string]: string;
 }
 
-function createDict(string: string) {
+export interface Maker<T> {
+    (p: T): string
+}
+function j2i(types: [string, string][], name: string) {
+    if(types.length===0)return `type ${name} = void;`
+    if (types.length === 1) {
+        return `type ${name} = ${types[0][1] || 'string'};`
+    }
+    return `interface ${name} {
+${types.reduce((result, [name, type]) => {
+        return [...result, `    ${name}: ${type || 'string'};`];
+    }, [] as string[]).join('\n')}
+}`
+}
+function createInterfaceJson(template: string) {
+    return Array.from(new Set(template.match(/\{(.+?)\}+/g) || []))
+        .map(s => s.replace(/[\{\}]/g, ''))
+        .map(s => s.split(":"))
+}
+export function genCode(name: string, template: string) {
+    const paramName = `${upperFirst(name)}Param`;
+    const text = j2i(createInterfaceJson(template) as any, paramName)
+    const result = `
+export ${text}
+export const ${name} = createFun<${paramName}>(\`${template}\`)
+`;
+    return result
+}
+function createDict(tmp: string) {
     const reg = /\{(.+?)\}+/g;
-    const dict = Array.from(new Set(string.match(reg) || []))
+    const dict = Array.from(new Set(tmp.match(reg) || []))
         .map(s => ({
-            name: s.replace(/[\{\}]/g, ''),
+            name: s.replace(/[\{\}]/g, '').split(":").shift() || '',
             target: new RegExp(`${s}`, 'g'),
         }))
         .reduce(
@@ -25,37 +52,28 @@ function createDict(string: string) {
         );
     return dict;
 }
-function createInterfaceJson(template: string) {
-    return Array.from(new Set(template.match(/\{(.+?)\}+/g) || []))
-        .map(s => ({
-            name: s.replace(/[\{\}]/g, ''),
-            target: 'string',
-        }))
-        .reduce(
-            (t, acc) => {
-                t[acc.name] = acc.target;
-                return t;
-            },
-            {} as StrParameter,
-        );
-
-}
-export function genCode(name: string, template: string) {
-    const paramName = `${upperFirst(name)}Param`;
-    const tmp = template.replace(/--.*\s*/g, '\n');
-    const text = last(j2i(createInterfaceJson(tmp) as any, { rootName: paramName }))
-    const result = `
-export ${text}
-export const ${name} = createFun<${paramName}>(\`${tmp}\`)
-`;
-    return result
-}
-
-export default function createFun<T = StrParameter>(string: string) {
+function createFun<T>(string: string) {
     const dict = createDict(string);
-    return (parameter: T) =>
-        Object.entries(parameter).reduce(
-            (str, [k, v]) => str.replace(dict[k], v),
-            string,
-        );
+    return ((parameter: T) => {
+        if (parameter) {
+            const type = typeof parameter;
+            switch (type) {
+                case 'bigint':
+                case 'boolean':
+                case 'number':
+                case 'string':
+                    return string.replace(Object.values(dict).pop() || '', (parameter as any).toString())
+                case 'object':
+                default:
+                    return Object.entries(parameter).reduce(
+                        (str, [k, v]) => str.replace(dict[k], v),
+                        string,
+                    )
+            }
+
+        } else {
+            return string;
+        }
+    }
+    ) as Maker<T>;
 }
